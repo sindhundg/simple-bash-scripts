0e36b2b4ac000bfa58337f29d0b9cf1f2e497909
ruanyf <yifeng.ruan@gmail.com>
2024-09-07 14:09:09 +0800

    refactor: fixed #68

--- scripts/while-read.sh
+++ scripts/while-read.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # while-read: read lines from a file
 count=0
-while read; do
-	printf "%d %s\n" $REPLY
+
+while read -r LINE; do
+	printf "%s| %s\n" "$count" "$LINE"
 	count=$(expr $count + 1)
 done <$1
